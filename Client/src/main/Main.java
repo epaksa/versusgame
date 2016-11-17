d2b99ce01c90463c98c7c1cc1fe512d3b57ef106
epaksa <epaksaepaksa@naver.com>
2016-11-17 11:06:32 +0900

    message 주고받는지 확인 완료

--- Client/src/main/Main.java
+++ Client/src/main/Main.java
@@ -5,49 +5,56 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
+import java.util.Scanner;
 
 public class Main {
 	
 	public static final int PORT = 5678;
 	public static final int BUF_SIZE = 65507;
-	public static final String IP = "52.78.185.203";
 	
 	static DatagramSocket ds = null;
+	static String peerInfo = null;
 	
 	public static void main(String[] args) {
 		
 		try {
 			ds = new DatagramSocket(PORT);
 			
-			sendToServer(ds);
-			String msg = receive(ds);
+			sendToServer(args[0]);
+			peerInfo = receive();
 			
-			Thread t1 = new Thread(new Runnable() {
+			Thread sendThread = new Thread(new Runnable() {
 				@Override
 				public void run() {
 					try {
-						sendToClient(ds, msg);
+						@SuppressWarnings("resource")
+						Scanner sc = new Scanner(System.in);
+						while(true){
+							sendToClient(sc.nextLine());
+						}
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+			});
+
+			Thread receiveThread = new Thread(new Runnable() {
+				@Override
+				public void run() {
+					try {
+						while(true){
+							receive();
+						}
 					} catch (IOException e) {
 						e.printStackTrace();
 					}
 				}
 			});
 			
-			Thread t2 = new Thread(new Runnable() {
-				@Override
-				public void run() {
-					try {
-						receive(ds);
-					} catch (IOException e) {
-						e.printStackTrace();
-					}
-				}
-			});
-			
-			t1.start();
-			t2.start();
-			t1.join();
-			t2.join();
+			sendThread.start();
+			receiveThread.start();
+			sendThread.join();
+			receiveThread.join();
 			
 		} catch (SocketException e) {
 			e.printStackTrace();
@@ -60,40 +67,40 @@ public class Main {
 		}
 	}
 
-	private static void sendToClient(DatagramSocket ds, String msg) throws IOException {
-		String[] split = msg.split(":");
+	private static void sendToClient(String msg) throws IOException {
+		String[] split = peerInfo.split(":");
 		String otherClientIP = split[0]; 
 		String otherClientPort = split[1];
 		InetAddress ip = InetAddress.getByName(otherClientIP);
 		int port = Integer.parseInt(otherClientPort.trim());
 		
 		// send
-		byte[] buffer = "hello".getBytes();
+		byte[] buffer = msg.getBytes();
 		DatagramPacket dp = new DatagramPacket(buffer, 0, buffer.length, ip, port);
 		ds.send(dp);
-		System.out.println("send to "+ip.getHostAddress()+":"+port);
+		
+		System.out.println("send ("+msg+") to "+ip.getHostAddress()+":"+port);
 	}
 
-	private static String receive(DatagramSocket ds) throws IOException {
+	private static String receive() throws IOException {
 		byte[] buffer = new byte[BUF_SIZE];
 		DatagramPacket dp = new DatagramPacket(buffer, 0, BUF_SIZE);
 		ds.receive(dp);
-		
-		System.out.println("*** received! ***");
 		
 		String receivedIp = dp.getAddress().getHostAddress();
 		int receivedPort = dp.getPort();
 		String receivedMsg = new String(dp.getData());
 		
-		System.out.println("ip:port - "+receivedIp+":"+receivedPort);
-		System.out.println("msg : "+receivedMsg);
+		peerInfo = receivedIp+":"+receivedPort;
+		
+		System.out.println("msg : "+receivedMsg+" ("+receivedIp+":"+receivedPort+")");
 		
 		return receivedMsg;
 	}
 
-	private static void sendToServer(DatagramSocket ds) throws IOException {
-		InetAddress server = InetAddress.getByName(IP);
-		byte[] msg = "This is message..".getBytes();
+	private static void sendToServer(String args) throws IOException {
+		InetAddress server = InetAddress.getByName(args);
+		byte[] msg = "message from client!".getBytes();
 		
 		DatagramPacket dp = new DatagramPacket(msg, 0, msg.length, server, PORT);
 		ds.send(dp);
